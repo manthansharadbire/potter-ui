4e43d2e3e0c32e7de072c42580bd5b37aa48f02d
Manthan Bire <manthansharadbire@gmail.com>
2025-04-05 11:39:00 +0530

    Character cards responsiveness added

--- src/components/CharCards.jsx
+++ src/components/CharCards.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React from "react";
 
-function CharCards({fullName, nickname,hogwartsHouse,interpretedBy,image,birthdate, }) {
+function CharCards({
+  fullName,
+  nickname,
+  hogwartsHouse,
+  interpretedBy,
+  image,
+  birthdate,
+}) {
   return (
-    <div className='justify-center flex'>
-    <div className='flex m-5 bg-red-500 rounded-lg w-[800px] cursor-pointer 
-     bg-gradient-to-r from-purple-600 via-pink-500 to-indigo-600 
-         shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl'>
-    <img src={image} alt="Character Card" className='h-[200px] w-[170px] rounded-l-lg ' />
-    <div className='flex-col m-5 rounded-l-lg justify-center'>
-    <h1 className="text-3xl font-semibold text-yellow-300">{fullName}</h1>
-    <h1  className=" text-yellow-300 text-xl">{nickname}</h1>
-    <p className='text-white'>{hogwartsHouse}</p>
-    <p className='text-white'>{interpretedBy}</p>
-    <p className='text-white'>{birthdate}</p>
+    <div className="flex justify-center m-6">
+      <div className="bg-gradient-to-r from-purple-600 via-pink-500 to-indigo-600 flex flex-col sm:flex-row max-w-[900px] w-full rounded-lg shadow-lg transform transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl cursor-pointer">
+        <img
+          src={image}
+          alt="Character Card"
+          className="h-[250px] w-full sm:h-[250px] sm:w-[200px] object-contain rounded-t-lg sm:rounded-l-lg sm:rounded-t-none shadow-lg shadow-lg"
+        />
+        <div className="flex flex-col justify-between p-6 sm:p-8 w-full">
+          <h1 className="text-2xl sm:text-3xl font-semibold text-yellow-300 text-center sm:text-left drop-shadow-lg">
+            {fullName}
+          </h1>
+
+          <h2 className="text-xl text-yellow-300 text-center sm:text-left drop-shadow-md">
+            {nickname}
+          </h2>
+
+          <p className="text-white text-lg text-center sm:text-left">
+            {hogwartsHouse}
+          </p>
+
+          <p className="text-white text-lg text-center sm:text-left">
+            {interpretedBy}
+          </p>
+
+          <p className="text-white text-lg text-center sm:text-left">
+            {birthdate}
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-    </div>
-  )
+  );
 }
 
 export default CharCards;
