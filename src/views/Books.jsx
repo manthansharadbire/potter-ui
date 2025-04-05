138ff18a6beeb31bfd55e6be34b8a2323bcdb31e
Manthan Bire <manthansharadbire@gmail.com>
2025-04-05 12:59:41 +0530

    all books fetched

--- src/views/Books.jsx
+++ src/views/Books.jsx
@@ -4,22 +4,8 @@ import BookCards from "../components/BookCards";
 
 function Books() {
 
-  const [books, setBooks] = useState([
-    {
-      number: 1,
-      title: "Harry Potter and the Sorcerer's Stone",
-      originalTitle: "Harry Potter and the Sorcerer's Stone",
-      releaseDate: "Jun 26, 1997",
-      description:
-        "On his birthday, Harry Potter discovers that he is the son of two well-known wizards, from whom he has inherited magical powers. He must attend a famous school of magic and sorcery, where he establishes a friendship with two young men who will become his companions on his adventure. During his first year at Hogwarts, he discovers that a malevolent and powerful wizard named Voldemort is in search of a philosopher's stone that prolongs the life of its owner.",
-      pages: 223,
-      cover:
-        "https://raw.githubusercontent.com/fedeperin/potterapi/main/public/images/covers/1.png",
-      index: 0,
-    },
-  ]);
-
-
+  const [books, setBooks] = useState([])
+  
 const fetchBooks = async () => {
   const response = await axios.get(`${import.meta.env.VITE_API_URL}/en/books`);
   setBooks(response.data);
